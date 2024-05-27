dbd5a36d64bdee61fc7759d42b2317f1b00fe7b2
Anjali <47922271+Anjali1208@users.noreply.github.com>
2024-05-27 21:03:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (54.44%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+      List<List<Integer>> ans=new ArrayList<>();
+       // Arrays.sort(nums);
+        helper(nums,0,ans,new ArrayList<>());
+        return ans ;
+    }
+
+    void helper(int[] nums,int ind,List<List<Integer>> ans,List<Integer> ds)
+    {
+        
+            ans.add(new ArrayList<>(ds));
+        
+
+        for(int i=ind;i<nums.length;i++)
+        {
+            if(i>ind && nums[i]==nums[i-1])
+            {
+                continue;
+            }
+            ds.add(nums[i]);
+            helper(nums,i+1,ans,ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+}
